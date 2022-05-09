51868550d75921da5287b8f4ff6978248233ce8f
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-05-10 03:26:45 +0600

    Load singleID API

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-   res.send('ok api running')
-});
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xabdk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-   const collection = client.db("test").collection("devices");
-   console.log('user connected');
-   // perform actions on the collection object
-   client.close();
+
+async function run() {
+
+   try {
+      await client.connect();
+      const productCollection = client.db('coatsBD').collection('product');
+
+      app.get('/product', async (req, res) => {
+         const query = {};
+         const cursor = productCollection.find(query);
+         const products = await cursor.toArray();
+         res.send(products);
+      });
+      app.get('/product/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const product = await productCollection.findOne(query);
+         res.send(product);
+      })
+
+   }
+   finally {
+
+   }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+   res.send('Running Genius Server');
 });
-
-
-
-
-
-
 
 app.listen(port, () => {
-   console.log('ok');
-});
+   console.log('Listening to port', port);
+})
